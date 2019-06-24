fd8f34a1aebd72a1a84dfc61f1a7776c9ad3af54
nancy <1243515312@qq.com>
2019-06-24 20:04:46 +0800

    修改厕位信息

--- src/pages/overview/toiletInfo.vue
+++ src/pages/overview/toiletInfo.vue
@@ -2,39 +2,62 @@
     <div class="render-div"> 
         <p class="dataTitle" style="margin-bottom:0;">今日数据</p>
         <div class="toiletInfo-container">
-            <el-row>
+            <el-row >
                 <el-col :span="12">
                     <div class="cinfo-content borderR contentHeight">
-                        <p class="info-title" v-if="dataInfo.length>0">{{dataInfo[0].positionName}}</p>
+                        <p class="info-title" v-if="firstDataInfo.length>0">{{firstDataInfo[0].positionName}}</p>
                         <el-row>
-                            <el-col :span="12" class="content" v-for="(item,index) in dataInfo.slice(0,2)" :key='index'>
-                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeName=='男厕所'?nanIcon:nvIcon" class="nanicon" /></p>
+                            <el-col :span="12" class="content" v-for="(item,index) in firstDataInfo" :key='index'>
+                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeId=='1'?nanIcon:nvIcon" class="nanicon" /></p>
                                 <div class="listContent">设备数：{{item.deviceCount}}</div>
                                 <div class="listContent">今日流量：{{item.amount}}</div>
                             </el-col>
-                            <!-- <el-col :span="12" class="content">
-                                <p class="left-title">女厕<img src="../../assets/img/nv.png" class="nanicon" /></p>
-                                <div class="listContent">设备数：1</div>
-                                <div class="listContent">今日流量：1</div>
-                            </el-col> -->
                         </el-row>
-                        <p class="noData" v-if="dataInfo.length==0">无数据</p>
+                        <p class="noData" v-if="firstDataInfo.length==0">无数据</p>
                     </div>
                 </el-col>
                 <el-col :span="12">
                     <div class="cinfo-content contentHeight">
-                        <p class="info-title" v-if="dataInfo.length>2">{{dataInfo[2].positionName}}</p>
+                        <p class="info-title" v-if="thirdDataInfo.length>0">{{thirdDataInfo[0].positionName}}</p>
                         <el-row>
-                            <el-col :span="12" class="content" v-for="(item,index) in dataInfo.slice(2,4)" :key='index'>
-                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeName=='男厕所'?nanIcon:nvIcon" class="nanicon" /></p>
+                            <el-col :span="12" class="content" v-for="(item,index) in thirdDataInfo" :key='index'>
+                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeId=='1'?nanIcon:nvIcon" class="nanicon" /></p>
                                 <div class="listContent">设备数：{{item.deviceCount}}</div>
                                 <div class="listContent">今日流量：{{item.amount}}</div>
                             </el-col>
                         </el-row>
-                        <p class="noData" v-if="dataInfo.length<=2">无数据</p>
+                        <p class="noData" v-if="thirdDataInfo.length==0">无数据</p>
                     </div>
                 </el-col>
             </el-row>
+            <!-- <el-row v-if="secondDataInfo.length>0 || forthDataInfo.length>0">
+                <el-col :span="12">
+                    <div class="cinfo-content borderR contentHeight">
+                        <p class="info-title" v-if="secondDataInfo.length>0">{{secondDataInfo[0].positionName}}</p>
+                        <el-row>
+                            <el-col :span="12" class="content" v-for="(item,index) in secondDataInfo" :key='index'>
+                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeId=='1'?nanIcon:nvIcon" class="nanicon" /></p>
+                                <div class="listContent">设备数：{{item.deviceCount}}</div>
+                                <div class="listContent">今日流量：{{item.amount}}</div>
+                            </el-col>
+                        </el-row>
+                        <p class="noData" v-if="secondDataInfo.length==0">无数据</p>
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    <div class="cinfo-content contentHeight">
+                        <p class="info-title" v-if="forthDataInfo.length>0">{{forthDataInfo[0].positionName}}</p>
+                        <el-row>
+                            <el-col :span="12" class="content" v-for="(item,index) in forthDataInfo" :key='index'>
+                                <p class="left-title">{{item.toiletTypeName}}<img :src="item.toiletTypeId=='1'?nanIcon:nvIcon" class="nanicon" /></p>
+                                <div class="listContent">设备数：{{item.deviceCount}}</div>
+                                <div class="listContent">今日流量：{{item.amount}}</div>
+                            </el-col>
+                        </el-row>
+                        <p class="noData" v-if="forthDataInfo.length==0">无数据</p>
+                    </div>
+                </el-col>
+            </el-row> -->
             <el-row>
                 <el-col :span="12">
                     <div class=" borderR contentHeight">
@@ -77,6 +100,10 @@ export default {
     data() {
         return{
             dataInfo:[],
+            firstDataInfo:[],
+            secondDataInfo:[],
+            thirdDataInfo:[],
+            forthDataInfo:[],
             nanIcon:require('../../assets/img/nan.png'),
             nvIcon:require('../../assets/img/nv.png'),
         }
@@ -90,6 +117,18 @@ export default {
             self.$http.get(self.api.getDeviceCountByToiletType, {}, function(response) {
                 if(response){
                     self.dataInfo = response.data;
+                    self.firstDataInfo = self.dataInfo.filter( e=>{
+                        return e.positionId == '1'
+                    })
+                    self.secondDataInfo = self.dataInfo.filter( e=>{
+                        return e.positionId == '2'
+                    })
+                    self.thirdDataInfo = self.dataInfo.filter( e=>{
+                        return e.positionId == '3'
+                    })
+                    self.forthDataInfo = self.dataInfo.filter( e=>{
+                        return e.positionId == '1'
+                    })
                 }else{
 
                 }
